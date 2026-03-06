75934eb2adc52a87fde5e82487353e347f234dbb
Bruno Augusto <brunoaugustomaciel@gmail.com>
2026-03-05 22:35:47 -0300

    feat - Refatora layout da Home para suportar diferentes estruturas de Row e Column, e implementa o método _layoutColunasLinhas.

--- lib/home.dart
+++ lib/home.dart
@@ -9,7 +9,10 @@ class Home extends StatelessWidget {
         title: Text("Tipos de Layout"),
       ),
       // body: _layoutListView(),
-      body: layoutGridView(),
+      // body: _layoutGridView(),
+      // body: _layoutContainerLinhas(),
+      // body: _layoutContainerColunas(),
+       body: _layoutColunasLinhas(),
     );
   }
 
@@ -25,19 +28,71 @@ class Home extends StatelessWidget {
   //  );
   // }
 
-  Widget? layoutGridView() {
-    return GridView(
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4, mainAxisSpacing: 10, crossAxisSpacing: 10),
-      children: [
-        SuaImagem(caminhoArquivo: "imagens/ADAAng.png"),
-        SuaImagem(caminhoArquivo: "imagens/Derac.jpg"),
-        SuaImagem(caminhoArquivo: "imagens/Elosport.png"),
-        SuaImagem(caminhoArquivo: "imagens/Guarani par.jpg"),
-        SuaImagem(caminhoArquivo: "imagens/IBIS.png"),
-        SuaImagem(caminhoArquivo: "imagens/OnzedeAgosto tatui.png"),
-        SuaImagem(caminhoArquivo: "imagens/Santos.jpg"),
-        SuaImagem(caminhoArquivo: "imagens/SaoBento.jpg"),
-      ]
+  // _layoutGridView() {
+  //   return GridView(
+  //     gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4, mainAxisSpacing: 10, crossAxisSpacing: 10),
+  //     children: [
+  //       SuaImagem(caminhoArquivo: "imagens/ADAAng.png"),
+  //       SuaImagem(caminhoArquivo: "imagens/Derac.jpg"),
+  //       SuaImagem(caminhoArquivo: "imagens/Elosport.png"),
+  //       SuaImagem(caminhoArquivo: "imagens/Guarani par.jpg"),
+  //       SuaImagem(caminhoArquivo: "imagens/IBIS.png"),
+  //       SuaImagem(caminhoArquivo: "imagens/OnzedeAgosto tatui.png"),
+  //       SuaImagem(caminhoArquivo: "imagens/Santos.jpg"),
+  //       SuaImagem(caminhoArquivo: "imagens/SaoBento.jpg"),
+  //     ]
+  //   );
+  // }
+
+  // _layoutContainerLinhas() {
+  //   return Container(
+  //     color: Colors.white,
+  //     child: Row(
+  //       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+  //       children: [
+  //         Text("Texto Exemplo"),
+  //         Text("bruno")
+  //       ]
+  //     )
+  //   );
+  // }
+
+  // _layoutContainerColunas() {
+  //   return Container(
+  //     color: Colors.redAccent,
+  //     child: Column(
+  //       mainAxisAlignment: MainAxisAlignment.start,
+  //       crossAxisAlignment: CrossAxisAlignment.start,
+  //       children: [
+  //         Text("Texto Exemplo"),
+  //         Text("bruno")
+  //       ]
+  //     )
+  //   );
+  // }
+
+  _layoutColunasLinhas() {
+    return Container(
+      color: Colors.white,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text("Texto Exemplo 1"),
+              Text("bruno")
+            ]
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Text("Texto Exemplo 2"),
+              Text("bruno")
+            ]
+          )
+        ]
+      )
     );
   }
 }
